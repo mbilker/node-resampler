24c7ae9b27fd0a754194b401c2912595c1dfe3aa
mbilker <me@mbilker.us>
2016-02-23 19:03:45 -0500

    Not working. Outputs weird static.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const binding = require('./build/Debug/resampler');
 const stream = require('stream');
 const util = require('util');
 
+const debug = require('debug')('resampler');
+
 function Resampler(inputRate, outputRate, quality) {
   if (quality == null) {
     quality = Resampler.QUALITY_HI;
@@ -9,15 +11,21 @@ function Resampler(inputRate, outputRate, quality) {
 
   stream.Transform.call(this);
 
+  debug('new', {inputRate, outputRate, quality});
+
   this.resampler = new binding.Resampler(inputRate, outputRate, quality);
 }
 util.inherits(Resampler, stream.Transform);
 
 Resampler.prototype._resample = function _resample(samples, callback) {
+  debug('_resample', {samples, samplesLength: samples.length});
+
   this.resampler.resample(samples, (err, resampled) => {
     if (err != null) {
       throw err;
     }
+
+    debug('_resample.sampled', {resampled, resampledLength: resampled.length});
 
     this.push(resampled);
     callback()
@@ -25,6 +33,8 @@ Resampler.prototype._resample = function _resample(samples, callback) {
 };
 
 Resampler.prototype._transform = function _transform(chunk, encoding, callback) {
+  //debug('_transform', {chunk, encoding});
+
   if (this.resampler.opened) {
     return this._resample(chunk, callback);
   }
@@ -39,6 +49,8 @@ Resampler.prototype._transform = function _transform(chunk, encoding, callback) 
 };
 
 Resampler.prototype._flush = function _flush(callback) {
+  debug('_flush');
+
   if (!this.resampler.opened) {
     return callback();
   }
--- resampler.cc
+++ resampler.cc
@@ -13,22 +13,24 @@
 
 using namespace v8;
 using namespace node;
-/*
-*/
 
-/*
-class ResamplerWorker : public AsyncWorker {
+class Resampler;
+class ResamplerWorker;
+
+class ResamplerWorker : public Nan::AsyncWorker {
   public:
-    ResamplerWorker(Resampler *rs, Callback *callback, char* inBuffer, int inBufferLength, char* prefix_, int prefixLength) : AsyncWorker(callback), 
+    ResamplerWorker(Resampler *rs, Nan::Callback *callback, v8::Local<v8::Object>& inBufferHandle, char* prefix_, int prefixLength)
+      : AsyncWorker(callback) {
+    }
 };
-*/
+
 
 class Resampler : public Nan::ObjectWrap {
 public:
   static NAN_MODULE_INIT(Init);
 
 protected:
-  Resampler() : ObjectWrap(),
+  Resampler() : Nan::ObjectWrap(),
     resampleHandle(NULL),
     opened(false),
     resampling(false),
@@ -69,25 +71,49 @@ protected:
   };
 
   struct ResampleBaton : Baton {
+    //v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> inBuffer;
+    char *inBuffer;
     char *inPtr;
+    size_t origInLength;
     size_t inLength;
 
-    v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> outBuffer;
+    //v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> outBuffer;
+    char *outBuffer;
     char *outPtr;
+    size_t origOutLength;
     size_t outLength;
 
     char *prefix;
     size_t prefixLength;
 
-    ResampleBaton(Resampler* rs_, Handle<Function> cb_, char* inBuffer_, int inBufferLength_, char* prefix_, int prefixLength_) :
-        Baton(rs_, cb_), inPtr(NULL), inLength(0), outPtr(NULL), outLength(0), prefix(NULL), prefixLength(0) {
-      Nan::HandleScope scope;
+    //ResampleBaton(Resampler* rs_, v8::Handle<v8::Function> cb_, v8::Handle<v8::Object> inBuffer_, char* prefix_, int prefixLength_) :
+    //    Baton(rs_, cb_), inPtr(NULL), inLength(0), outPtr(NULL), outLength(0), prefix(NULL), prefixLength(0) {
+    ResampleBaton(Resampler* rs_, v8::Handle<v8::Function> cb_, char *inBuffer_, int inBufferLength_, char* prefix_, int prefixLength_) :
+        Baton(rs_, cb_),
+        inBuffer(NULL),
+        inPtr(NULL),
+        origInLength(0),
+        inLength(0),
+        outBuffer(NULL),
+        outPtr(NULL),
+        origOutLength(0),
+        outLength(0),
+        prefix(NULL),
+        prefixLength(0)
+    {
+      //Nan::HandleScope scope;
 
+      //if (!inBuffer_.IsEmpty()) {
       if (inBufferLength_ > 0 && inBuffer_ != NULL) {
         // Persist the input buffer and save pointer
+        //inBuffer.Reset(v8::Isolate::GetCurrent(), inBuffer_);
+        //inPtr = Buffer::Data(inBuffer_);
+        //inLength = Buffer::Length(inBuffer_);
+        origInLength = inBufferLength_;
         inLength = inBufferLength_;
-        inPtr = static_cast<char *>(malloc(inLength));
-        memcpy(inPtr, inBuffer_, inLength);
+        inBuffer = static_cast<char *>(malloc(inLength));
+        inPtr = inBuffer;
+        memcpy(inBuffer, inBuffer_, inLength);
       }
 
       if (prefixLength_ > 0 && prefix_ != NULL) {
@@ -102,152 +128,58 @@ protected:
 
       if (totalLength > 0) {
         // Create outBuffer based on factor + pad and save pointer
-        outLength = totalLength * rs->factor + RS_BUFFER_PAD;
-        v8::Local<v8::Object> localOutBuffer = Nan::NewBuffer(outLength).ToLocalChecked();
-        outBuffer.Reset(v8::Isolate::GetCurrent(), localOutBuffer);
-        outPtr = Buffer::Data(localOutBuffer);
+        origOutLength = totalLength * rs->factor + RS_BUFFER_PAD;
+        outLength = origOutLength;
+        //v8::Local<v8::Object> localOutBuffer = Nan::NewBuffer(outLength).ToLocalChecked();
+        //outBuffer.Reset(v8::Isolate::GetCurrent(), localOutBuffer);
+        //outPtr = Buffer::Data(localOutBuffer);
+        outBuffer = static_cast<char *>(malloc(outLength));
+        outPtr = outBuffer;
       }
     }
     virtual ~ResampleBaton() {
       //if (inPtr != NULL) free(inPtr);
-      if (outPtr != NULL) outBuffer.Reset();
+      //if (outPtr != NULL) outBuffer.Reset();
+      if (inBuffer != NULL) free(inBuffer);
+      if (outBuffer != NULL) free(outBuffer);
       if (prefix != NULL) free(prefix);
     }
   };
 
   struct FlushBaton : Baton {
-    v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> outBuffer;
-    char* outPtr;
+    //v8::Persistent<v8::Object, v8::CopyablePersistentTraits<v8::Object>> outBuffer;
+    char *outBuffer;
+    char *outPtr;
+    size_t origOutLength;
     size_t outLength;
 
-    FlushBaton(Resampler* rs_, v8::Handle<v8::Function> cb_) :
-        Baton(rs_, cb_), outPtr(NULL), outLength(0) {
-      Nan::HandleScope scope;
+    FlushBaton(Resampler* rs_, v8::Local<v8::Function> cb_) :
+        Baton(rs_, cb_), outBuffer(NULL), outPtr(NULL), origOutLength(0), outLength(0) {
+      //Nan::HandleScope scope;
 
       // Create outBuffer and save pointer
-      outLength = rs->factor * RS_BUFFER_PAD;
+      origOutLength = rs->factor * RS_BUFFER_PAD;
+      outLength = origOutLength;
 
-      v8::Local<v8::Object> localOutBuffer = Nan::NewBuffer(outLength).ToLocalChecked();
-      outBuffer.Reset(v8::Isolate::GetCurrent(), localOutBuffer);
-      outPtr = Buffer::Data(localOutBuffer);
+      //v8::Local<v8::Object> localOutBuffer = Nan::NewBuffer(outLength).ToLocalChecked();
+      //outBuffer.Reset(v8::Isolate::GetCurrent(), localOutBuffer);
+      //outPtr = Buffer::Data(localOutBuffer);
+      outBuffer = static_cast<char *>(malloc(outLength));
+      outPtr = outBuffer;
     }
     virtual ~FlushBaton() {
-      if (outPtr != NULL) outBuffer.Reset();
+      //if (outPtr != NULL) outBuffer.Reset();
+      if (outBuffer != NULL) {
+        outPtr = NULL;
+        free(outBuffer);
+      }
     }
   };
 
-  static NAN_METHOD(New) {
-    if (!info.IsConstructCall())
-      return Nan::ThrowTypeError("Use the new operator");
-
-    REQUIRE_ARGUMENTS(3);
-
-    Resampler* rs = new Resampler();
-    rs->Wrap(info.This());
-
-    rs->factor = info[1]->NumberValue() / info[0]->NumberValue();
-    rs->quality = info[2]->Int32Value();
-
-    info.GetReturnValue().Set(info.This());
-  }
-
-  static NAN_METHOD(Open) {
-    OPTIONAL_ARGUMENT_FUNCTION(0, callback);
-
-    Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
-
-    COND_ERR_CALL(rs->opened, callback, "Already open");
-    COND_ERR_CALL(rs->closing, callback, "Still closing");
-
-    rs->resampleHandle = resample_open(rs->quality, rs->factor, rs->factor);
-    COND_ERR_CALL(rs->resampleHandle == 0, callback, "Couldn't open");
-    rs->opened = true;
-
-    if (!callback.IsEmpty() && callback->IsFunction())  {
-      Local<Value> argv[0] = { };
-      TRY_CATCH_CALL(info.Holder(), callback, 0, argv);
-    }
-
-    info.GetReturnValue().Set(info.Holder());
-  }
-
-  static NAN_METHOD(Close) {
-    OPTIONAL_ARGUMENT_FUNCTION(0, callback);
-
-    Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
-
-    COND_ERR_CALL(!rs->opened, callback, "Not open");
-    COND_ERR_CALL(rs->resampling, callback, "Still resampling");
-    COND_ERR_CALL(rs->flushing, callback, "Still flushing");
-    COND_ERR_CALL(rs->closing, callback, "Still closing");
-
-    rs->closing = true;
-    resample_close(rs->resampleHandle);
-    rs->opened = false;
-    rs->closing = false;
-
-    if (!callback.IsEmpty() && callback->IsFunction())  {
-      Local<Value> argv[0] = { };
-      TRY_CATCH_CALL(info.Holder(), callback, 0, argv);
-    }
-
-    info.GetReturnValue().Set(info.Holder());
-  }
-
-  static NAN_METHOD(Resample) {
-    REQUIRE_ARGUMENTS(2);
-    REQUIRE_ARGUMENT_FUNCTION(1, callback);
-
-    Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
-
-    COND_ERR_CALL(!rs->opened, callback, "Not open");
-    COND_ERR_CALL(rs->resampling, callback, "Already resampling");
-    COND_ERR_CALL(!Buffer::HasInstance(info[0]), callback, "First arg must be a buffer");
-
-    // Initialize leftovers if we haven't yet.
-    if (rs->leftovers == NULL) {
-      rs->leftovers = static_cast<char *>(malloc(RS_SAMPLE_BYTES));
-      rs->leftoversLength = 0;
-    }
-
-    char *chunkPtr = Buffer::Data(info[0]);
-    size_t chunkLength = Buffer::Length(info[0]);
-
-    int totalLength = chunkLength + rs->leftoversLength;
-    int totalSamples = totalLength / RS_SAMPLE_BYTES;
-
-    ResampleBaton* baton;
-
-    if (totalSamples < 0) {
-      // Copy entire chunk into leftovers
-      memcpy(rs->leftovers + rs->leftoversLength, chunkPtr, chunkLength);
-      rs->leftovers += chunkLength;
-      baton = new ResampleBaton(rs, callback, chunkPtr, chunkLength, NULL, 0);
-    } else {
-      uint32_t newLeftoversLength = totalLength % RS_SAMPLE_BYTES;
-      uint32_t inBufferLength = totalLength - rs->leftoversLength - newLeftoversLength;
-
-      // Slice buffer to correct length
-      //if (inBufferLength > 0) {
-      //  Local<Function> slice = Local<Function>::Cast(args[0]->ToObject()->Get(Nan::New<String>("slice")));
-      //  Local<Value> sliceArgs[2] = { Nan::New<Integer>(0), Nan::New<Integer>(inBufferLength) };
-      //  inBuffer = slice->Call(args[0]->ToObject(), 2, sliceArgs)->ToObject();
-      //}
-
-      // Create baton and let it memcpy leftovers
-      baton = new ResampleBaton(rs, callback, chunkPtr, inBufferLength, rs->leftovers, rs->leftoversLength);
-
-      // Copy new leftovers
-      if (newLeftoversLength > 0) memcpy(rs->leftovers, (chunkPtr + chunkLength) - newLeftoversLength, newLeftoversLength);
-      rs->leftoversLength = newLeftoversLength;
-    }
-
-    rs->resampling = true;
-
-    uv_queue_work(uv_default_loop(), &baton->request, DoResample, (uv_after_work_cb)AfterResample);
-
-    info.GetReturnValue().Set(info.Holder());
-  }
+  static NAN_METHOD(New);
+  static NAN_METHOD(Open);
+  static NAN_METHOD(Close);
+  static NAN_METHOD(Resample);
 
   static NAN_METHOD(Flush) {
     REQUIRE_ARGUMENTS(1);
@@ -331,13 +263,11 @@ protected:
     v8::Local<v8::Object> outBuffer;
 
     if (baton->outPtr != NULL) {
-      //Local<Function> slice = Local<Function>::Cast(baton->outBuffer->Get(Nan::New<String>("slice")));
-      //Local<Value> sliceArgs[2] = { Nan::New<Integer>(0), Nan::New<Integer>(origOutLength - baton->outLength) };
-      //outBuffer = slice->Call(baton->outBuffer, 2, sliceArgs)->ToObject();
-      v8::Local<v8::Object> batonOutBuffer = Nan::New(baton->outBuffer);
-      size_t outLength = node::Buffer::Length(batonOutBuffer) - baton->outLength;
-      char *data = node::Buffer::Data(batonOutBuffer);
-      outBuffer = Nan::CopyBuffer(data, outLength).ToLocalChecked();
+      //v8::Local<v8::Object> batonOutBuffer = Nan::New(baton->outBuffer);
+      //char *origBufferData = node::Buffer::Data(batonOutBuffer);
+      //size_t outLength = node::Buffer::Length(batonOutBuffer) - baton->outLength;
+      size_t outLength = baton->origOutLength - baton->outLength;
+      outBuffer = Nan::CopyBuffer(baton->outBuffer, outLength).ToLocalChecked();
     } else {
       outBuffer = Nan::NewBuffer(0).ToLocalChecked();
     }
@@ -379,14 +309,18 @@ protected:
     //Local<Value> sliceArgs[2] = { Nan::New<Integer>(0), Nan::New<Integer>(origOutLength - baton->outLength) };
     //Local<Object> outBuffer = slice->Call(baton->outBuffer, 2, sliceArgs)->ToObject();
 
-    v8::Local<v8::Object> batonOutBuffer = Nan::New(baton->outBuffer);
-    char *origBufferData = node::Buffer::Data(batonOutBuffer);
-    size_t origOutLength = node::Buffer::Length(batonOutBuffer) - baton->outLength;
-    v8::Local<v8::Object> outBuffer = Nan::CopyBuffer(origBufferData, origOutLength).ToLocalChecked();
+    //v8::Local<v8::Object> batonOutBuffer = Nan::New(baton->outBuffer);
+    //char *origBufferData = node::Buffer::Data(batonOutBuffer);
+    //size_t origOutLength = node::Buffer::Length(batonOutBuffer) - baton->outLength;
+    //v8::Local<v8::Object> outBuffer = Nan::CopyBuffer(origBufferData, origOutLength).ToLocalChecked();
+    size_t outLength = baton->origOutLength - baton->outLength;
+    v8::Local<v8::Object> outBuffer = Nan::CopyBuffer(baton->outBuffer, outLength).ToLocalChecked();
     v8::Local<v8::Value> argv[2] = { Nan::Null(), outBuffer };
 
     rs->flushing = false;
+
     TRY_CATCH_CALL(rs->handle(), baton->callback, 2, argv);
+
     delete baton;
   }
 
@@ -417,6 +351,128 @@ NAN_MODULE_INIT(Resampler::Init) {
 
   Nan::SetAccessor(tpl->InstanceTemplate(), Nan::New<String>("opened").ToLocalChecked(), OpenedGetter, 0);
   Nan::Set(target, Nan::New("Resampler").ToLocalChecked(), Nan::GetFunction(tpl).ToLocalChecked());
+}
+
+NAN_METHOD(Resampler::New) {
+  if (!info.IsConstructCall())
+    return Nan::ThrowTypeError("Use the new operator");
+
+  REQUIRE_ARGUMENTS(3);
+
+  Resampler* rs = new Resampler();
+  rs->Wrap(info.This());
+
+  rs->factor = info[1]->NumberValue() / info[0]->NumberValue();
+  rs->quality = info[2]->Int32Value();
+
+  info.GetReturnValue().Set(info.This());
+}
+
+NAN_METHOD(Resampler::Open) {
+  OPTIONAL_ARGUMENT_FUNCTION(0, callback);
+
+  Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
+
+  COND_ERR_CALL(rs->opened, callback, "Already open");
+  COND_ERR_CALL(rs->closing, callback, "Still closing");
+
+  rs->resampleHandle = resample_open(rs->quality, rs->factor, rs->factor);
+  COND_ERR_CALL(rs->resampleHandle == 0, callback, "Couldn't open");
+  rs->opened = true;
+
+  if (!callback.IsEmpty() && callback->IsFunction())  {
+    Local<Value> argv[0] = { };
+    TRY_CATCH_CALL(info.Holder(), callback, 0, argv);
+  }
+
+  info.GetReturnValue().Set(info.Holder());
+}
+
+NAN_METHOD(Resampler::Close) {
+  OPTIONAL_ARGUMENT_FUNCTION(0, callback);
+
+  Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
+
+  COND_ERR_CALL(!rs->opened, callback, "Not open");
+  COND_ERR_CALL(rs->resampling, callback, "Still resampling");
+  COND_ERR_CALL(rs->flushing, callback, "Still flushing");
+  COND_ERR_CALL(rs->closing, callback, "Still closing");
+
+  rs->closing = true;
+  resample_close(rs->resampleHandle);
+  rs->opened = false;
+  rs->closing = false;
+
+  if (!callback.IsEmpty() && callback->IsFunction())  {
+    Local<Value> argv[0] = { };
+    TRY_CATCH_CALL(info.Holder(), callback, 0, argv);
+  }
+
+  info.GetReturnValue().Set(info.Holder());
+}
+
+NAN_METHOD(Resampler::Resample) {
+  REQUIRE_ARGUMENTS(2);
+  REQUIRE_ARGUMENT_FUNCTION(1, callback);
+
+  Resampler* rs = ObjectWrap::Unwrap<Resampler>(info.Holder());
+
+  COND_ERR_CALL(!rs->opened, callback, "Not open");
+  COND_ERR_CALL(rs->resampling, callback, "Already resampling");
+  COND_ERR_CALL(!Buffer::HasInstance(info[0]), callback, "First arg must be a buffer");
+
+  // Initialize leftovers if we haven't yet.
+  if (rs->leftovers == NULL) {
+    rs->leftovers = static_cast<char *>(malloc(RS_SAMPLE_BYTES));
+    rs->leftoversLength = 0;
+  }
+
+  char *chunkPtr = Buffer::Data(info[0]);
+  size_t chunkLength = Buffer::Length(info[0]);
+
+  int totalLength = chunkLength + rs->leftoversLength;
+  int totalSamples = totalLength / RS_SAMPLE_BYTES;
+
+  ResampleBaton* baton;
+
+  char *inBuffer;
+  int inBufferLength;
+
+  if (totalSamples < 0) {
+    // Copy entire chunk into leftovers
+    memcpy(rs->leftovers + rs->leftoversLength, chunkPtr, chunkLength);
+    rs->leftovers += chunkLength;
+    baton = new ResampleBaton(rs, callback, NULL, 0, NULL, 0);
+  } else {
+    size_t newLeftoversLength = totalLength % RS_SAMPLE_BYTES;
+    size_t newInBufferLength = totalLength - rs->leftoversLength - newLeftoversLength;
+
+    // Slice buffer to correct length
+    if (newInBufferLength > 0) {
+      //Local<Function> slice = Local<Function>::Cast(info[0]->ToObject()->Get(Nan::New<String>("slice")));
+      //Local<Value> sliceArgs[2] = { Nan::New<Integer>(0), Nan::New<Integer>(inBufferLength) };
+      //inBuffer = slice->Call(args[0]->ToObject(), 2, sliceArgs)->ToObject();
+      //inBuffer = Nan::CopyBuffer(chunkPtr, inBufferLength).ToLocalChecked();
+      inBuffer = chunkPtr;
+      inBufferLength = newInBufferLength;
+    } else {
+      inBuffer = static_cast<char *>(malloc(0));
+      inBufferLength = 0;
+    }
+
+    // Create baton and let it memcpy leftovers
+    baton = new ResampleBaton(rs, callback, inBuffer, inBufferLength, rs->leftovers, rs->leftoversLength);
+
+    // Copy new leftovers
+    if (newLeftoversLength > 0) memcpy(rs->leftovers, (chunkPtr + chunkLength) - newLeftoversLength, newLeftoversLength);
+    rs->leftoversLength = newLeftoversLength;
+  }
+
+  rs->resampling = true;
+
+  uv_queue_work(uv_default_loop(), &baton->request, DoResample, (uv_after_work_cb)AfterResample);
+
+  info.GetReturnValue().Set(info.Holder());
 }
 
 void NodeInit(Handle<Object> exports) {
